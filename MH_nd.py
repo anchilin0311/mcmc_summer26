799891485ca87b6304ad90e6161b5e459e36c295
qwert12345-1 <anchilin0311@gmail.com>
2026-07-02 01:34:24 -0400

    mh-nd with 3d donut and 3d bimodal distribution done

--- MH_nd.py
+++ MH_nd.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # starting point (need to fix the dimension to match M-H)
 # x0 = np.array([10, 10]) # now just 2d
-x0 = np.array([5,0,0])
+x0 = np.array([0,0,0])
 
 def metropolis_hastings(target_distribution, proposal_distribution, q, x_0, iter):
 
@@ -36,10 +36,22 @@ def target_distribution_normal_2d(x):
 
 # bimodal 2d
 def target_distribution_bimodal_2d(x):
-    return (
-        0.8 * np.exp(-0.5 * ((x[0] + 2) / 0.8)**2) * np.exp(-0.5 * ((x[1] + 2) / 0.8)**2)
-        + 0.2 * np.exp(-0.5 * ((x[0] - 3) / 0.5)**2) * np.exp(-0.5 * ((x[1] - 3) / 0.5)**2)
-    )
+    return (0.8 * np.exp(-0.5 * ((x[0] + 2) / 0.8)**2) * np.exp(-0.5 * ((x[1] + 2) / 0.8)**2)
+        + 0.2 * np.exp(-0.5 * ((x[0] - 3) / 0.5)**2) * np.exp(-0.5 * ((x[1] - 3) / 0.5)**2))
+
+# bimodal 3d
+"""
+the center should have two modes (-2,-2,-2) and (3,3,1), with different weights. 
+the samples would only concentrate around the mode that x0 is closer to. 
+(play around x0=[0,0,0], x0 = [-1,0,0], and x0=[5,0,0])
+"""
+def target_distribution_bimodal_3d(x):
+    m1 = np.exp(-0.5 * (((x[0] + 2) / 0.8)**2 + ((x[1] + 2) / 0.8)**2 + ((x[2] + 2) / 0.8)**2))
+
+    m2 = np.exp(-0.5 * (((x[0] - 3) / 0.6)**2 + ((x[1] - 3) / 0.6)**2 + ((x[2] - 1) / 0.6)**2))
+
+    return 0.7 * m1 + 0.3 * m2
+
 
 # donut 2d
 def target_distribution_donut_2d(x, R=3, sigma=0.4):
@@ -114,18 +126,109 @@ def mh_2d_plot(samples, target_distribution, step=50, bins = 100, pause_time=0.5
 
     plt.show()
 
+def mh_3d_plot(samples, step=20, bins=80, pause_time=0.5):
+    x_samples = samples[:, 0]
+    y_samples = samples[:, 1]
+    z_samples = samples[:, 2]
+
+    x_min, x_max = x_samples.min() - 1, x_samples.max() + 1
+    y_min, y_max = y_samples.min() - 1, y_samples.max() + 1
+    z_min, z_max = z_samples.min() - 1, z_samples.max() + 1
+
+    fig = plt.figure(figsize=(10, 8))
+
+    graphs = fig.add_gridspec(2, 2, width_ratios=(4, 1), height_ratios=(1, 4), hspace=0.05, wspace=0.05)
+
+    # 3D scatter plot
+    plot_3d = fig.add_subplot(graphs[1, 0], projection='3d')
+    # x_scatter = fig.add_subplot(graphs[1, 0])
+    x_hist = fig.add_subplot(graphs[0, 0])
+    y_hist = fig.add_subplot(graphs[1, 1])
+    z_hist = fig.add_subplot(graphs[0, 1])
+
+    for i in range(step, len(samples), step):
+        current = samples[:i]
+
+        x_current = current[:, 0]
+        y_current = current[:, 1]
+        z_current = current[:, 2]
+
+        plot_3d.clear()
+        x_hist.clear()
+        y_hist.clear()
+        z_hist.clear()
+
+
+        plot_3d.scatter(
+            x_current,
+            y_current,
+            z_current,
+            s=3,
+            alpha=0.5, 
+            marker = "o",
+            label = "samples",
+            color = "orange")
+        
+        plot_3d.scatter(
+            x_current[-1],
+            y_current[-1],
+            z_current[-1],
+            s=100,
+            color = "purple",
+            label = "current",
+            marker = "o"
+        )
+        
+        plot_3d.set_xlabel("x")
+        plot_3d.set_ylabel("y")
+        plot_3d.set_zlabel("z")
+        # plot_3d.set_title("3D MH")
+
+        plot_3d.set_xlim(x_min, x_max)
+        plot_3d.set_ylim(y_min, y_max)
+        plot_3d.set_zlim(z_min, z_max)
+        plot_3d.legend()
+
+        # x marginal
+        x_hist.hist(x_current, bins=bins, density=True, alpha=0.5, orientation = "vertical")
+        x_hist.set_xlabel("x")
+        x_hist.set_ylabel("density")
+        # x_hist.set_xlim(x_min, x_max)
+        x_hist.set_ylim(0, 1)
+
+        #y marginal
+        y_hist.hist(y_current, bins=bins, density=True, alpha=0.5, orientation = "horizontal")
+        y_hist.set_xlabel("density")
+        y_hist.set_ylabel("y")
+        y_hist.set_ylim(y_min, y_max)
+
+        #z marginal
+        z_hist.hist(z_current, bins=bins, density=True, alpha=0.5, orientation = "vertical")
+        z_hist.set_xlabel("z")
+        z_hist.set_ylabel("density")
+        z_hist.set_ylim(0, 1)
+
+        fig.suptitle(f"Samples till now: {i}")
+
+        plt.pause(pause_time)
+
+
+    plt.tight_layout()
+    plt.show()
 
 samples, acceptance_rate = metropolis_hastings(
-    target_distribution_donut_2d,
+    target_distribution_bimodal_3d,
     proposal_distribution_normal_2d,
     q_standard_normal_2d,
     x_0=x0,
     iter=100000
 )
 
-mh_2d_plot(
-    samples,
-    target_distribution_donut_2d,
-    step=500,
-    pause_time=0.2
-)
+# mh_2d_plot(
+#     samples,
+#     target_distribution_donut_2d,
+#     step=500,
+#     pause_time=0.2
+# )
+
+mh_3d_plot(samples, step=3000, bins=1000, pause_time=0.1)
